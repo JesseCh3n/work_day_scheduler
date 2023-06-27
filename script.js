62f317c77e3444ad43a05f52d98f1eb6a64348e5
JesseCh3n <105155682+JesseCh3n@users.noreply.github.com>
2023-06-27 18:32:56 +1000

    Added comments

--- script.js
+++ script.js
@@ -32,12 +32,12 @@ $(function () {
     return schedules;
   }
   
-  // Display current day and existing project
+  // Display current day and existing activities
   function saveScheduleToStorage(schedules) {
     localStorage.setItem('schedules', JSON.stringify(schedules));
   }
   
-  // Gets project data from local storage and displays it
+  // Gets activities data from local storage and displays them
   function printSchedule() {
     // clear current schedule on the page
     scheduleDisplayEl.empty();
@@ -49,11 +49,11 @@ $(function () {
     for (var i = 0; i < 9; i += 1) {
       const schedule = schedules[i];
       const hours = dayjs().hour(9+i);
-      console.log(hours.hour());
-      console.log(typeof(hours.hour()));
+      //console.log(hours.hour());
+      //console.log(typeof(hours.hour()));
       const hour = dayjs().hour();
-      console.log(hour);
-      console.log(typeof(hour));
+      //console.log(hour);
+      //console.log(typeof(hour));
   
       // Create row and columns for the page
       const rowEl = $('<div class="row time-block"></div>');
@@ -82,25 +82,25 @@ $(function () {
     }
   }
 
-  // Removes a project from local storage and prints the project data
+  // Handle saving activities
   function handleScheduleSubmit(event) {
     event.preventDefault();
 
     // read user input from the input
     const scheduleIndex = parseInt($(this).attr('data-index'));
     const scheduleText = $(this).parent().children("textarea").val();
-    console.log(scheduleText);
-    console.log(typeof(scheduleText));
+    //console.log(scheduleText);
+    //console.log(typeof(scheduleText));
   
     const newItem = {
       text: scheduleText,
       index: scheduleIndex,
     };
-    console.log(newItem);
+    // console.log(newItem);
     // add schedule to local storage
     const schedules = readSchedulesFromStorage();
     schedules.splice(scheduleIndex, 1, newItem);
-    console.log(schedules);
+    // console.log(schedules);
     saveScheduleToStorage(schedules);
   
   }
@@ -110,24 +110,4 @@ $(function () {
 
   scheduleDisplayEl.on('click', '.saveBtn', handleScheduleSubmit);
 
-
-
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
 });
